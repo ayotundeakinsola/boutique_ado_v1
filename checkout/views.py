06280637c4ea9cc652b960eb1cbf0385a44e7c3b
Ayotunde Akinsola <ayotundeakinsola@gmail.com>
2020-08-12 14:51:21 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HFL7XI8fHhdhdDr4GBLcyMmnXYYPnYvlxvSa7i9Y9AuJclh0KfM4qIEH8E9InpTqCN6Aqf4SbSl4pP0LDm2urIg00q1uGjzUD',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
